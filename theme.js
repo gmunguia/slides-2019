fd090fb2e36dd507d8deac992bce7215a83218bd
Gerardo Munguia <gerardomunguiahoyo@gmail.com>
2019-04-20 16:05:41 +0200

    💅

---
+++ theme.js
@@ -0,0 +1,10 @@
+export default {
+  // add a custom font
+  font: 'Open Sans, sans-serif',
+  // custom colors
+  colors: {
+    text: '#c4e4f2',
+    background: '#272727',
+    link: '#2a6496',
+  },
+}
